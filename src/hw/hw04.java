5bd9957696c76819c065e50e429adaf1b98fcf34
SamaLin <105021058@live.asia.edu.tw>
2016-11-23 17:49:22 +0800

    week08-20161127-SamaLin

--- src/hw/hw04.java
+++ src/hw/hw04.java
@@ -2,14 +2,93 @@ package hw;
 /*
  * Topic: 請寫以類別方式寫一個程式可顯示四位數字的 LED 燈一組，可根據輸入的四位數字顯示數字。比如說， 由鍵盤輸入 1234 ，則可在螢幕上顯示如圖
  * Date: 2016/11/21
- * Author: 1050210XX 周永振老師
+ * Author: 105021058 蕭懋霖
  */
 
 public class hw04 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn=new Scanner(System.in);
+		int a=scn.nextInt();
+		int cach=0;
+		int caca[]=new int [4];
+		for(int k=0;k<4;k++){
+			caca[3-k]=(a/(int)Math.pow(10, k))%10;
+			}
+		String led[][]={
+				
+				{"*****",
+				 "*   *",
+				 "*   *",
+				 "*   *",
+				 "*****"},
+				 
+				 {"    *",
+				  "    *",
+				  "    *",
+				  "    *",
+				  "    *"},
+				  
+				 {"*****",
+				  "    *",
+				  "*****",
+				  "*    ",
+				  "*****"},
+				  
+				 {"*****",
+				  "    *",
+				  "*****",
+				  "    *",
+			          "*****"},
+			      
+				 {"*   *",
+			          "*   *",
+			          "*****",
+				  "    *",
+				  "    *"},
+				  
+				 {"*****",
+				  "*    ",
+				  "*****",
+				  "    *",
+				  "*****"},
+				  
+				 {"*****",
+				  "*    ",
+				  "*****",
+				  "*   *",
+				  "*****"},
+				  
+				 {"*****",
+				  "    *",
+				  "    *",
+				  "    *",
+				  "    *"},
+				  
+				 {"*****",
+				  "*   *",
+				  "*****",
+				  "*   *",
+				  "*****"},
+				  
+				  {"*****",
+				   "*   *",
+				   "*****",
+				   "    *",
+				   "*****"}
+				  
+		};
+//		System.out.println(led[0][1]);
+//		System.out.println(led[1][0]);
+//		for(int k=0;k<4;k++){
+			for(int i=0;i<5;i++){
+				for(int j=0;j<4;j++){
+				System.out.print(led[caca[j]][i]+" ");
+				}
+				System.out.println();
+			}
+//		}
 	}
 
 }
